f0ec371c4c416b352759844b863d884aded1eeb0
Arti Mauryaa <artimaurya9721@gmail.com>
2024-06-04 10:59:24 +0530

    add-filter-update

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
+import menu from './Menu'
+// import '../node_modules/bootstrap/dist/js/bootstrap.js'
+
+function App() {
+
+  const [item , setItem] = useState(menu)
+
+  const filterItem = (cate)=>{
+
+    const updateItem = menu.filter((curElem)=>{
+       return (curElem.category === cate);
+    });
+   
+    setItem(updateItem);
+
+    
+  
+    // console.log(updateItem)
+    // console.log(data)
+  }
+  
+
+  // const search = (evt)=>{
+  //   const data = evt.target.value
+  //   return 
+  //   // console.log(data)
+  // }
+  return (
+    <>
+
+    <div className="heading_style">
+    <h1>Order Your Favourite dish</h1>
+      <button className='btn btn-warning mx-3' onClick={()=>filterItem("lunch")}>Lucnch</button>
+      <button className='btn btn-warning mx-3' onClick={()=>filterItem("breakfast")}>Breakfast</button>
+      <button className='btn btn-warning mx-3' onClick={()=>filterItem("dinner")}>Dinner</button>
+      <button className='btn btn-warning mx-3' onClick={()=>filterItem("evening")}>Evening</button>
+      <button className='btn btn-warning mx-3' onClick={()=>setItem(menu)}>All</button>
+      {/* <input type="text" placeholder='search item' onChange={search}/> */}
+    </div>
+  <div className="contain">
+      {
+        item.map((data)=>{
+          // console.log(data)
+          
+          const {id , img , name , price , category, description} = data
+            return(          
+             <div className="cards " key= {id}>
+              <div className="card" >
+                  <img src={img} alt="mypic" className='card_img'/>
+                  <div className="card_info">
+                      <span className="card_category">{description}</span>
+                      <h3 className='card_title'>{name}</h3>
+                      <p>{category}</p>
+                      <p>{price}</p>
+                      <a href="" target='_blank'>
+                          <button className='btn1'>Order Now</button>
+                      </a>
+                  </div>
+              </div>
+             </div>
+            )
+        })
+      }
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Menu.jsx
@@ -0,0 +1,83 @@
+const menu = [
+    {
+        id : "1",
+        img : "images/breakfast2.jpg",
+        name : "bread",
+        category : "breakfast",
+        price : "112$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "2",
+        img : "images/dinner3.jpg",
+        name : "nan deli",
+        category : "dinner",
+        price : "122$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "3",
+        img : "images/evening3.jpg",
+        name : "samosa",
+        category : "evening",
+        price : "129$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "4",
+        img : "images/breakfast_Retro.jpg",
+        name : "bread",
+        category : "breakfast",
+        price : "92$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "5",
+        img : "images/evening2.jpg",
+        name : "samosa",
+        category : "evening",
+        price : "125$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "6",
+        img : "images/lunch1.jpg",
+        name : "dal roti",
+        category : "lunch",
+        price : "82$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "7",
+        img : "images/dinner2.jpg",
+        name : "paneer",
+        category : "dinner",
+        price : "102$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "8",
+        img : "images/evening4.jpg",
+        name : "pakaudi",
+        category : "evening",
+        price : "132$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "9",
+        img : "images/dinner1.jpg",
+        name : "roti sabji",
+        category : "dinner",
+        price : "142$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        id : "10",
+        img : "images/lunch2.jpg",
+        name : "dal matthi",
+        category : "lunch",
+        price : "112$",
+        description :" Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+]
+export default menu
